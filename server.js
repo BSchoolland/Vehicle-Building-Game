5c2310dfaa0328b4dc6b1eca79681c90e92a90b5
Benjamin Schoolland <bschoolland@gmail.com>
2024-02-12 11:17:15 -0800

    fix: get port from env

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+// Use the PORT environment variable if it's available
+const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
-// Serve the index.html file when requests are made to the root of the server (localhost:3000/)
+// Serve the index.html file when requests are made to the root of the server
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });

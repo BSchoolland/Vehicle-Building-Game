5abfc20db6cd425b0d9cafb62e42127ead7da78a
Benjamin Schoolland <bschoolland@gmail.com>
2024-03-29 15:57:08 -0700

    feat: navigation to and from changeLog

--- public/js/game/index.js
+++ public/js/game/index.js
@@ -8,43 +8,8 @@ button.addEventListener('click', () => {
     }
     window.location.href = 'levels.html';
 });
-
-const noEditor = true; // since the editor is not implemented yet, use a greyed out button that says "coming soon"
-
-function createHTML() {
-    const container = document.getElementById('container');
-    // clear the container
-    container.innerHTML = '';
-    // Create elements
-    const h1 = document.createElement('h1');
-    const editorLink = document.createElement('a');
-    const campaignLink = document.createElement('a');
-
-    // Set attributes and content
-    container.className = 'container';
-    h1.textContent = 'Welcome to Wrecking Wheels!';
-    if (noEditor) {
-        editorLink.className = 'button disabled';
-        editorLink.textContent = 'Editor (coming soon)';
-    }
-    else {
-        editorLink.href = 'editor.html';
-        editorLink.className = 'button';
-        editorLink.textContent = 'Editor';
-    }
-    campaignLink.href = 'levels.html';
-    campaignLink.className = 'button';
-    campaignLink.textContent = 'Levels';
-
-    // Append elements
-    container.appendChild(h1);
-    container.appendChild(campaignLink);
-    container.appendChild(editorLink);
-    
-
-    // Append the container to the body (or another parent element)
-    document.body.appendChild(container);
-}
-
-
-// createHTML();
+// when the other button is clicked, go to the change log
+let changeButton = document.getElementById('change-log');
+changeButton.addEventListener('click', () => {
+    window.location.href = 'changeLog.html';
+});

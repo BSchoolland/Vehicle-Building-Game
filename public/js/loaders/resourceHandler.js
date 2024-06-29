5a0ba0251739dfb6888b0b5684a721717195e42e
Benjamin Schoolland <bschoolland@gmail.com>
2024-06-29 12:26:39 -0700

    feat: create resource handler

---
+++ public/js/loaders/resourceHandler.js
@@ -0,0 +1,42 @@
+// handles recording and loading player resources from local storage
+// FIXME: integrate teh crazy games SDK for storing resources to player accounts instead of just local storage
+
+const worldCount = 4; // FIXME: this should be dynamic based on the number of worlds in the game
+// each world has its own set of obtainable resources
+// resources consist of parts that the player can use to build their contraption to beat the world's boss
+class ResourceHandler {
+    constructor() {
+        this.resources = {};
+        this.loadResources();
+    }
+    async loadResources() {
+        for (let i = 1; i <= worldCount; i++) {
+            this.resources[i] = await this.loadWorldResources(i);
+        }
+    }
+    async loadWorldResources(worldNum) {
+        if (localStorage.getItem(`world${worldNum}Resources`)) {
+            return JSON.parse(localStorage.getItem(`world${worldNum}Resources`));
+        } else {
+            // create a new resource object with nothing loaded
+            let resources = {};
+            // save this to local storage
+            localStorage.setItem(`world${worldNum}Resources`, JSON.stringify(resources));
+            return resources; 
+        }
+    }
+    getWorldResources(worldNum) {
+        return this.resources[worldNum];
+    }
+    addBlockToResources(worldNum, blockName, count = 1) {
+        let resources = this.resources[worldNum];
+        if (resources[blockName]) {
+            resources[blockName] += count;
+        } else {
+            resources[blockName] = count;
+        }
+        localStorage.setItem(`world${worldNum}Resources`, JSON.stringify(resources));
+    }
+}
+
+export default ResourceHandler;

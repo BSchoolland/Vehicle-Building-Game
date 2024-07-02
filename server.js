b0ae55dedcf25b2bec4dd0030367eabe9e3279a4
Benjamin Schoolland <bschoolland@gmail.com>
2024-07-02 11:49:32 -0700

    fix: a bug where levels without bonus objectives would not be synced to the server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const userRoutes = require('./routes/userRoutes'); // Import your routes
 const fs = require('fs');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // environment variables
 require("dotenv").config();
